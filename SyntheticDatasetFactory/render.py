c574b0bb5679ab4e51c9622dbd162270bbb7468c
Theo Morales <tml@trifork.com>
2019-02-26 15:12:56 +0100

    Add TODO and clean up the code

--- SyntheticDatasetFactory/render.py
+++ SyntheticDatasetFactory/render.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 import numpy as np
 import moderngl
@@ -8,9 +8,29 @@ from moderngl.ext.obj import Obj
 from PIL import Image
 
 
+'''
+    ----- TODO -----
+
+- Random positioning of the gate
+- Boundaries definition for the gate
+- Camera calibration (use the correct parameters)
+- Project on transparent background
+- Overlay with background image
+- Histogram equalization of both images (hue, saturation, luminence ?...)
+- Motion blur (shader ?)
+- Anti alisasing (shader ?)
+- Ship it!
+
+'''
+
+# Parameters
+width = 640
+height = 480
+
+
 # Data files
-vertex_data = Obj.open('data/gate_triangulated_faces.obj').pack()
-texture_image = Image.open('data/wood.jpg')
+vertex_data = Obj.open('data/gate_1mx1m_150cm.obj').pack()
+texture_image = Image.open('data/shiny-white-metal-texture.jpg')
 vertex_shader_source = open('data/shader.vert').read()
 fragment_shader_source = open('data/shader.frag').read()
 
@@ -20,43 +40,36 @@ ctx = moderngl.create_standalone_context()
 # Shaders
 prog = ctx.program(vertex_shader=vertex_shader_source, fragment_shader=fragment_shader_source)
 
-# Matrices and Uniforms
-translation = Vector3()
-translation += [-45.0, 25.0, 0.0] # Translate the mesh
+# Model translation, rotation, scaling
+translation = Vector3([0.0, 0.0, 0.0])
+rotation = Quaternion.from_y_rotation(np.pi / 2.0)
+scale = Vector3([1., 1., 1.]) # Scale it by a factor of 1
+model = Matrix44.from_translation(translation) * rotation * Matrix44.from_scale(scale)
 
-orientation = Quaternion()
-rotation = Quaternion.from_z_rotation(np.pi / 4.0) # Rotate about Z by pi/2
-orientation = rotation * orientation
-
-scale = Vector3([50., 50., 50.]) # Scale it by a factor of 3
-
-transRotScaleMatrix = Matrix44.identity()
-transRotScaleMatrix = transRotScaleMatrix * Matrix44.from_translation(translation)
-transRotScaleMatrix = transRotScaleMatrix * orientation # Multiply matrices and quaternions directly
-transRotScaleMatrix = transRotScaleMatrix * Matrix44.from_scale(scale)
-
-perspective = Matrix44.perspective_projection(
+# Perspective projection
+projection = Matrix44.perspective_projection(
     45.0, # field of view in y direction in degrees
-    1.0, # aspect ratio of the view (width / height)
+    width/height, # aspect ratio of the view
     0.01, # distance from the viewer to the near clipping plane (only positive)
     1000.0 # distance from the viewer to the far clipping plane (only positive)
 )
-print("perspective projection: \n{}".format(perspective))
-lookat = Matrix44.look_at(
-    (0, -250, 400), # eye: position of the camera in world coordinates
+
+# Camera view matrix
+view = Matrix44.look_at(
+    (0, 15, 20), # eye: position of the camera in world coordinates
     (0.0, 0.0, 0.0), # target: position in world coordinates that the camera is looking at
     (0.0, 0.0, 1.0), # up: up vector of the camera
 )
-print("lookat: \n{}".format(lookat))
 
-mvp = perspective * lookat * transRotScaleMatrix
-print("model view projection: \n{}".format(mvp))
+# Model View Projection matrix
+mvp = projection * view * model
 
+# Shader program
 prog['Light'].value = (-140.0, -300.0, 350.0)
 prog['Color'].value = (1.0, 1.0, 1.0, 0.25)
 prog['Mvp'].write(mvp.astype('f4').tobytes())
 
-# Texture
+# Texturing
 texture = ctx.texture(texture_image.size, 3, texture_image.tobytes())
 texture.build_mipmaps()
 
@@ -66,8 +79,8 @@ vao = ctx.simple_vertex_array(prog, vbo,* ['in_vert', 'in_text', 'in_norm'])
 
 # Framebuffers
 fbo = ctx.framebuffer(
-    ctx.renderbuffer((512, 512)),
-    ctx.depth_renderbuffer((512, 512)),
+    ctx.renderbuffer((width, height)),
+    ctx.depth_renderbuffer((width, height)),
 )
 
 # Rendering
