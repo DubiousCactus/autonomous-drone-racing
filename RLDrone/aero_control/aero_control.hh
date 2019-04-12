46dc7193ddb460bbe5c7044809076f1543e41b46
Theo Morales <tml@trifork.com>
2019-04-12 16:57:07 +0200

    Fix build err

--- RLDrone/aero_control/aero_control.hh
+++ RLDrone/aero_control/aero_control.hh
@@ -16,7 +16,7 @@ namespace gazebo
 	class AeroControl: public ModelPlugin
 	{
 		public:
-			AeroControl(): ModelPlugin()
+			AeroControl(): ModelPlugin();
 			void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/);
 			void OnUpdate();
 			void SetVelocity(const ignition::math::Vector3d vel);
